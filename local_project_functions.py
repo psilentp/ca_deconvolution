3fceceb6a81d49703cf457d264d0deb7a1550273
psilentp <thlindsay1@gmail.com>
2018-03-30 15:17:36 -0700

    added local_project_function.py with forward optimization function.

---
+++ local_project_functions.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def forward_optimize(resampled_ca,resample_times,
+                     kernel,
+                     kernel_window_s = 2,
+                     kernel_gain = 0.05):
+    kernel = kernel*kernel_gain
+    recon = np.zeros_like(resample_times)
+    impulses = np.zeros_like(resample_times)
+    kernel_window_idx = np.int(kernel_window_s*resample_rate)
+    max_idx = len(resample_times)
+    last_imp = np.argwhere(impulse_idxs < end_first_loop)[-1][0]
+    for imp_idx,nxt_idx in zip(impulse_idxs[:last_imp],impulse_idxs[1:last_imp+1]):
+        imp_window = impulses[imp_idx:imp_idx+kernel_window_idx]
+        n1 = np.linalg.norm(recon[imp_idx:nxt_idx]-resampled_ca[imp_idx:nxt_idx])
+        recon[imp_idx:imp_idx+kernel_window_idx] += kernel[:kernel_window_idx]
+        n2 = np.linalg.norm(recon[imp_idx:nxt_idx] - resampled_ca[imp_idx:nxt_idx])
+        if n2>n1:
+            recon[imp_idx:imp_idx+kernel_window_idx] -= kernel[:kernel_window_idx]
+        else:
+            impulses[imp_idx] = 1
+    #finish up - bit slower
+    for imp_idx,nxt_idx in zip(impulse_idxs[last_imp:-2],impulse_idxs[last_imp+1:-1]):
+        kw = max_idx-imp_idx
+        imp_window = impulses[imp_idx:imp_idx+kw]
+        n1 = np.linalg.norm(recon[imp_idx:nxt_idx]-resampled_ca[imp_idx:nxt_idx])
+        recon[imp_idx:imp_idx+kw] += kernel[:kw]
+        n2 = np.linalg.norm(recon[imp_idx:nxt_idx] - resampled_ca[imp_idx:nxt_idx])
+        if n2>n1:
+            recon[imp_idx:imp_idx+kw] -= kernel[:kw]
+        else:
+            impulses[imp_idx] = 1
+    return recon,impulses
