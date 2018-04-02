ab83b701af789127ca45abeae4c81779aeda9907
psilentp <thlindsay1@gmail.com>
2018-04-02 12:35:23 -0700

    tried optimizing the forward deconvolution method against the test data set

--- local_project_functions.py
+++ local_project_functions.py
@@ -1,15 +1,19 @@
 import numpy as np
 
 
-def forward_optimize(resampled_ca,resample_times,
+def forward_optimize(resampled_ca,
+                     resample_times,
+                     impulse_idxs,
                      kernel,
                      kernel_window_s = 2,
                      kernel_gain = 0.05):
     kernel = kernel*kernel_gain
     recon = np.zeros_like(resample_times)
     impulses = np.zeros_like(resample_times)
+    resample_rate = resample_times[1]-resample_times[0]
     kernel_window_idx = np.int(kernel_window_s*resample_rate)
     max_idx = len(resample_times)
+    end_first_loop = resample_times[-1] - kernel_window_s
     last_imp = np.argwhere(impulse_idxs < end_first_loop)[-1][0]
     for imp_idx,nxt_idx in zip(impulse_idxs[:last_imp],impulse_idxs[1:last_imp+1]):
         imp_window = impulses[imp_idx:imp_idx+kernel_window_idx]
